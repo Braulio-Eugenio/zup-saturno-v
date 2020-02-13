0c148bab0365dbb36849061441e7da73b8d362d0
Braulio-Eugenio <braulio.eugenio@outlook.com>
2020-02-13 17:54:33 -0300

    Alteração

--- src/br/com/zup/estrelas/saturnov/Programa.java
+++ src/br/com/zup/estrelas/saturnov/Programa.java
@@ -6,7 +6,7 @@ public class Programa {
 
 	public static void main(String[] args) {
 		Scanner taclado = new Scanner(System.in);
-System.out.println("");
+System.out.println("Hello World");
 	}
 
 }
