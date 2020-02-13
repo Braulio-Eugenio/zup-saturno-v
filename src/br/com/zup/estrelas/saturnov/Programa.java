4d86b36256fcd5005ca807176112d3a56bea3b5d
Braulio-Eugenio <braulio.eugenio@outlook.com>
2020-02-13 17:49:58 -0300

    Hello World

--- src/br/com/zup/estrelas/saturnov/Programa.java
+++ src/br/com/zup/estrelas/saturnov/Programa.java
@@ -1,5 +1,12 @@
 package br.com.zup.estrelas.saturnov;
 
+import java.util.Scanner;
+
 public class Programa {
+
+	public static void main(String[] args) {
+		Scanner taclado = new Scanner(System.in);
+System.out.println("");
+	}
 
 }
