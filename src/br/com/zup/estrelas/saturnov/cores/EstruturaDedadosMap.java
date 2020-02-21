923bdf2b2db2ba31deb2a628fabc7b6c9d896917
Braulio-Eugenio <braulio.eugenio@outlook.com>
2020-02-21 13:22:51 -0300

    Nova atualização

--- src/br/com/zup/estrelas/saturnov/cores/EstruturaDedadosMap.java
+++ src/br/com/zup/estrelas/saturnov/cores/EstruturaDedadosMap.java
@@ -11,19 +11,29 @@ public class EstruturaDedadosMap {
 		String nome;
 		String cpf;
 
-		for (int i = 0; i < 10; i++) {
-			System.out.println("Digite "+(i+1)+ "º nome :");
+		for (int i = 0; i < 3; i++) {
+			System.out.println("Digite " + (i + 1) + "º nome :");
 			nome = teclado.nextLine();
 			System.out.println("Digite o cpf :");
 			cpf = teclado.nextLine();
 			estruturaMap.put(cpf, nome);
-			
+
 		}
+		for (String mostrarNome : estruturaMap.values()) {
+			System.out.println(mostrarNome);
 
+		}
+		System.out.println("Par remover um usuario digite um cpf :");
+		cpf = teclado.nextLine();
+		estruturaMap.remove(cpf);
+		System.out.println("USUARIO REMOVIDO.\nOs usuario atuais são: ");
+		for (String mostrarNome : estruturaMap.values()) {
+			System.out.println( mostrarNome);
+		}
+		
 	}
-
 }
-/**
+/*
  * 2. Escreva um programa para criar um HashMap onde a chave é o CPF e o valor o
  * Nome. Leia do usuário 10 conjuntos de valores e armazene em seu Hashmap
  * Mostre na tela os 10 valores de nomes armazenados Peça ao usuário um CPF e
